ff929ac7718ca20c61f9f300aff20cd09980452f
unknown <hichamsskouti@gmail.com>
2024-02-04 18:57:10 +0100

    adding the fiend list and css stylng

--- Eat-and-split/src/App.js
+++ Eat-and-split/src/App.js
@@ -21,5 +21,44 @@ const initialFriends = [
 
 
 export default function App(){
-  return<div>ello </div>
+  return(<div className="app">
+    <div className="sidebar">
+    <FriendList/>
+
+    </div>
+  </div>)
+}
+
+function FriendList(){
+  const friends= initialFriends;
+  return <ul>
+    {friends.map((friend)=>
+    <Friend friend={friend} key={friend.id}></Friend>
+    )}
+  </ul>
+}
+
+function Friend({friend}){
+  return(<li>
+      <img src={friend.image} alt={friend.name}></img>
+      <h3>{friend.name}</h3>
+      {friend.balance<0 && (
+        <p className="red">
+          You owe {friend.name} {Math.abs(friend.balance)} euro
+        </p>
+      )}
+
+{friend.balance>0 && (
+        <p className="green">
+          Your {friend.name} owes you {Math.abs(friend.balance)} euro
+        </p>
+      )}
+
+{friend.balance===0 && (
+        <p >
+          You and Your {friend.name} are even
+        </p>
+      )}
+      <button className="button">Select</button>
+  </li>)
 }

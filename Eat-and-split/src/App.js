83866dc9f11507d297cbc4b97e9a25c31b836f44
unknown <hichamsskouti@gmail.com>
2024-02-09 21:07:10 +0100

    adding state to the form component

--- Eat-and-split/src/App.js
+++ Eat-and-split/src/App.js
@@ -120,21 +120,26 @@ function FormAddFriend({onAddFriend}){
 }
 
 function FormSplitBill({selectedFriend}){
+
+  const [bill,setBill]=useState('');
+  const [paidByUser,setPaidByUser]=useState('');
+  const [whoIsPaying,setWhoIsPaying]=useState('user');
+
   return(
 <form className="form-split-bill">
 <h2>Split a bill with {selectedFriend.name}</h2>
 
 <label>Bill value</label>
-<input type="text" ></input>
+<input type="text" value={bill} onChange={(e)=>setBill(e.target.value)}></input>
 
 <label>Your expense</label>
-<input type="text"></input>
+<input type="text" value={paidByUser} onChange={(e)=>setPaidByUser(e.target.value)} ></input>
 
 <label>{selectedFriend.name}'s expense</label>
 <input type="text" disabled></input>
 
 <label>Who is paying the bill</label>
-<select>
+<select value={whoIsPaying} onChange={(e)=>setWhoIsPaying(e.target.value)}>
     <option value="user">You</option>
     <option value="friend">{selectedFriend.name}</option>
 

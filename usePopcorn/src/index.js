748618d0ea682f9d6aa1499b2d1d9c0450a68368
unknown <hichamsskouti@gmail.com>
2024-02-10 16:04:48 +0100

    Building a Reusable Star Rating Component

--- usePopcorn/src/index.js
+++ usePopcorn/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+//import './index.css';
+//import App from './App';
 
+import StarRating from './StarRating';
 
 
 
@@ -11,7 +12,10 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+   { /*<App />*/}
+   <StarRating maxRating={10}/>
+   <StarRating maxRating={5}/>
+   <StarRating />
   </React.StrictMode>
 );
 

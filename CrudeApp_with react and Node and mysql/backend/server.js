77126a05867083c396646d9f752391c0c57de288
unknown <hichamsskouti@gmail.com>
2024-02-16 18:51:59 +0100

    setting up a new simple crude app with node js and sql

---
+++ CrudeApp_with react and Node and mysql/backend/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql");
+
+const app = express();
+
+app.use(cors());
+
+app.listen(8081,()=>{
+    console.log("lestening");
+})

f263ab2f04d27fc55c8ad46eb65804e581a91519
unknown <hichamsskouti@gmail.com>
2024-02-16 19:17:37 +0100

    setting up server.js and the connrction to sql table

--- CrudeApp_with react and Node and mysql/backend/server.js
+++ CrudeApp_with react and Node and mysql/backend/server.js
@@ -6,6 +6,25 @@ const app = express();
 
 app.use(cors());
 
+
+const db = mysql.createConnection({
+
+    host:"localhost",
+    user:"root",
+    password:"",
+    database:"crud"
+})
+
+app.get("/",(req,res)=>{
+    const  sql = "SELECT * FROM student";
+
+    db.query(sql,(err,data)=>{
+        if(err)return res.json("error");
+        return res.json(data);
+    })
+})
+
+
 app.listen(8081,()=>{
     console.log("lestening");
 })

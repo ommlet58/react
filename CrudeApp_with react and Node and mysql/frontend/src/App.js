9a9cc3d011be311b00655eae9303a41b4d7c283e
unknown <hichamsskouti@gmail.com>
2024-02-17 01:30:23 +0100

    restart the front end

--- CrudeApp_with react and Node and mysql/frontend/src/App.js
+++ CrudeApp_with react and Node and mysql/frontend/src/App.js
@@ -1,17 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {BrowserRouter,Routes, Route} from "react-router-dom";
-import Student from './Student';
+
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Student/>}></Route>
-      </Routes>
-      </BrowserRouter>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }

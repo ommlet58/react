a74b517e6fc0a46a468ad495158214a6e36c6564
unknown <hichamsskouti@gmail.com>
2024-02-20 04:03:55 +0100

    creat the cakander component and getting the plugins

---
+++ Admin DashBoard/admin/src/scenes/calendar/index.jsx
@@ -0,0 +1,27 @@
+
+import { formatDate } from '@fullcalendar/core';
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
+import interactionPlugin from '@fullcalendar/interaction';
+import {
+    Box,
+    List,
+    ListItem,
+    ListItemText,
+    Typography,
+    useTheme
+} from "@mui/material";
+import Header from '../../components/Header';
+import {token} from "../../theme";
+
+
+
+const Calendar = () => {
+    
+
+return(<Box></Box>);
+  };
+  
+  export default Calendar;

d6183c2f04526ca933e4e6b36f57bb3a7de8212a
unknown <hichamsskouti@gmail.com>
2024-02-20 01:21:53 +0100

    contacts table

--- Admin DashBoard/admin/src/App.js
+++ Admin DashBoard/admin/src/App.js
@@ -4,9 +4,9 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import DashBoard  from "./scenes/dashboard";
 import { Routes, Route } from "react-router-dom";
 import Sidebar from "./scenes/global/Sidebar";
-/*import Team from "./scenes/team";
-import Invoices from "./scenes/invoices";
+import Team from "./scenes/team";
 import Contacts from "./scenes/contacts";
+/*import Invoices from "./scenes/invoices";
 import Bar from "./scenes/bar";
 import Form from "./scenes/form";
 import Line from "./scenes/line";
@@ -29,9 +29,9 @@ function App() {
               <Topbar/>
               <Routes>
                    <Route path="/" element={<DashBoard></DashBoard>}></Route>
-                   {/*<Route path="/team" element={<Team></Team>}></Route>
-                   <Route path="/contacts" element={<Contacts></Contacts>}></Route>
-                   <Route path="/invoices" element={<Invoices></Invoices>}></Route>
+                   <Route path="/team" element={<Team></Team>}></Route>
+                  <Route path="/contacts" element={<Contacts></Contacts>}></Route>
+                   {/* <Route path="/invoices" element={<Invoices></Invoices>}></Route>
                    <Route path="/form" element={<Form></Form>}></Route>
                    <Route path="/bar" element={<Bar></Bar>}></Route>
                    <Route path="/pie" element={<Pie></Pie>}></Route>
--- Admin DashBoard/admin/src/scenes/dashboard/index.jsx
+++ Admin DashBoard/admin/src/scenes/dashboard/index.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Header from '../../components/Header'
+import {Box} from "@mui/material"
 
 function DashBoard() {
   return (
-    <div>DashBoard</div>
+    <Box m="20px">
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+
+      <Header title="DASHBOARD" subtitle="Welcome to your dashboard"></Header>
+      </Box>
+    </Box>
   )
 }
 

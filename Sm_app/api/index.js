9f6d538a066158e0217bb2e7d3671d088720de9f
unknown <hichamsskouti@gmail.com>
2024-02-22 02:18:37 +0100

    adding the cotrollers and routes

--- Sm_app/api/index.js
+++ Sm_app/api/index.js
@@ -1,7 +1,18 @@
 import Express from "express";
+import authRouts from "./routes/auth.js";
+import userRouts from "./routes/users.js";
+import postRouts from "./routes/posts.js";
+import likeRouts from "./routes/likes.js";
+import commentRouts from "./routes/comments.js";
 const app = Express();
 
+app.use("/api/auth",authRouts);
+app.use("/api/users",userRouts);
+app.use("/api/posts",postRouts);
+app.use("/api/comments",commentRouts);
+app.use("/api/likes",likeRouts);
 
 app.listen(8800,()=>{
     console.log("api");
 })
+
---
+++ Sm_app/api/routes/posts.js
@@ -0,0 +1,9 @@
+import express from "express";
+import {} from "../controllers/post";
+
+const router = express.Router();
+
+router.get("",)
+
+
+export default router 

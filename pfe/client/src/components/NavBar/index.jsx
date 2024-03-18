0aee0fb6fb9f9c961f53b24c71342439b4ac40e4
unknown <hichamsskouti@gmail.com>
2024-03-18 02:30:29 +0000

    navbar

---
+++ pfe/client/src/components/NavBar/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function NavBar() {
+  return (<>
+    <nav className='nav-style'>
+      <ul>
+        <li><a href='#'>Aloha</a></li>
+        <li><a href='#'>Location</a></li>
+        <li><a href='#'>Activities</a></li>
+        <li><a href='#'>Surf Level</a></li>
+        <li><a href='#'>Communnty</a></li>
+        <li><a href='#'>Package</a></li>
+        <li><a href='#'>Shop</a></li>
+      </ul>
+    </nav>
+    
+    </>
+  )
+}
+
+export default NavBar
